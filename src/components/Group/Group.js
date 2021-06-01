32630a5ba2ec12b28dc4198e523463a6ce0e7728
Swaroop <breadfan18@gmail.com>
2021-05-31 21:25:01 -0400

    broke down Teams in its own component

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -1,15 +1,32 @@
 import style from './Group.module.css';
+import Team from '../../components/Team/Team';
+import { sortableContainer, sortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
 
 const Group = (props) => {
+
+    // const onlyTeams = [];
+    // props.group.map(teams =>
+    //     onlyTeams.push(teams.name)
+    // )
+
+    // console.log(onlyTeams);
+
+
     return (
         <article className={style.groupCard}>
             <div className={style.groupName}>{props.groupLetter}</div>
             {
                 props.group.map((team, idx) => (
-                    <div className={style.eachTeam} key={idx}>
-                        <p>{team.name}</p>
-                        <p>{team.code}</p>
-                    </div>
+                    // <div className={style.eachTeam} key={idx}>
+                    //     <p>{team.name}</p>
+                    //     <p>{team.code}</p>
+                    // </div>
+                    <Team 
+                    team={team} 
+                    key={team}
+                    />
                 ))
             }
         </article>
---
+++ src/components/Team/Team.js
@@ -0,0 +1,10 @@
+import style from './Team.module.css';
+
+const Team = (props) => (
+     <div className={style.eachTeam}>
+                        <p>{props.team.name}</p>
+                        <p>{props.team.code}</p>
+                    </div>
+)
+
+export default Team;
