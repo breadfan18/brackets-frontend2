07bc01f5a16259c2fde653f2871c54b7b70fbd56
Swaroop <breadfan18@gmail.com>
2021-06-10 22:31:49 -0400

    reverted changes to handlePointsCalc

--- src/App.js
+++ src/App.js
@@ -29,7 +29,13 @@ export default function App() {
       quartersPicks: {},
       semisPicks: {},
       finalPick: String,
-      totalPoints: {},
+      totalPoints: {
+        groupStage: null,
+        roundOf16: null,
+        quarters: null,
+        semis: null,
+        final: null,
+      },
       uid: String
     },
     pickSaved: false
@@ -120,22 +126,25 @@ export default function App() {
   }
 
   function handlePointsCalc() {
-    let userPicksFinalObj = {};
-    let counter = 0;
+    let userPicksFinalObj = [];
+    let resultsFinalObj = groupStandings[0];
     let streamlinedUserPicks = userPicks.allPicks[0][0];
 
+    let counter1 = 0;
     for (const group in streamlinedUserPicks) {
       let teamsShortArr = [];
-      if (Object.hasOwnProperty.call(streamlinedUserPicks, group) && counter < 8) {
+      if (Object.hasOwnProperty.call(streamlinedUserPicks, group) && counter1 < 8) {
         const teamsFullArr = streamlinedUserPicks[group];
         teamsFullArr.map(team => teamsShortArr.push(team.name));
         userPicksFinalObj[group] = teamsShortArr;
-        counter++;
+        counter1++;
       }
     }
 
-    console.log('Picksss', userPicksFinalObj);
-    console.log('Results', groupStandings[0]);
+    console.log('Results: ', resultsFinalObj);
+    console.log('Picksss: ', userPicksFinalObj);
+    
+
   }
 
   handlePointsCalc();
