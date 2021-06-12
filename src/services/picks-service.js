c06ec395222ec10e52448d666c85b808ded460e3
Swaroop <breadfan18@gmail.com>
2021-06-12 15:17:20 -0400

    corrected heroku url

--- src/services/picks-service.js
+++ src/services/picks-service.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://brackets-backend.herokuapp.com/api/picks';
-const BASE_URL_ALL = 'https://brackets-backend.herokuapp.com/api/picks';
+const BASE_URL_ALL = 'https://brackets-backend.herokuapp.com/api/picks/all';
 // const BASE_URL = 'http://localhost:3001/api/picks';
 // const BASE_URL_ALL = 'http://localhost:3001/api/picks/all';
 
