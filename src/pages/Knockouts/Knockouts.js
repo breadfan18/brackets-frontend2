6d337b51d69122b0c298c164c7fb230d7e262237
Swaroop <breadfan18@gmail.com>
2021-06-14 16:57:32 -0400

    completed rendering roundOf16 games

--- src/pages/Knockouts/Knockouts.js
+++ src/pages/Knockouts/Knockouts.js
@@ -5,34 +5,39 @@ const Knockouts = (props) => {
     let winnersArr = [];
     let runnerUpsArr = [];
     let counter = 0;
-    
+
     for (const arr in grpResults) {
-      if (counter < 8) {
-        let winners = grpResults[arr].slice(0,1);
-        let runnerUps = grpResults[arr].slice(1,2);
-        winnersArr.push(winners[0]);
-        runnerUpsArr.push(runnerUps[0]);
-      }
-      counter++;
+        if (counter < 8) {
+            let winners = grpResults[arr].slice(0, 1);
+            let runnerUps = grpResults[arr].slice(1, 2);
+            winnersArr.push(winners[0]);
+            runnerUpsArr.push(runnerUps[0]);
+        }
+        counter++;
     }
 
     return (
         <div className={styles.knockoutsContainer}>
             <h4>Knockout Round Picks</h4>
-            <section className={styles.round16Section}>
-                {
-                    winnersArr.map((each, idx) => 
-                        <article className={styles.gameArticle}>
-                        <h6 className={styles.gameNum}>Game {idx+1}</h6>
-                        <div className={styles.koTeams}>{each}</div>
-                        <div className={styles.vs}>VS</div>
-                        <div className={styles.koTeams}>{
-                            idx%2 === 0 ? runnerUpsArr[idx+1] : runnerUpsArr[idx-1]
-                        }</div>
-                    </article>
-                    )
-                }
-            </section>
+            {
+                grpResults ?
+                    <section className={styles.round16Section}>
+                        {
+                            winnersArr.map((each, idx) =>
+                                <article className={styles.gameArticle}>
+                                    <h6 className={styles.gameNum}>Game {idx + 1}</h6>
+                                    <div className={styles.koTeams}>{each}</div>
+                                    <div className={styles.vs}>VS</div>
+                                    <div className={styles.koTeams}>{
+                                        idx % 2 === 0 ? runnerUpsArr[idx + 1] : runnerUpsArr[idx - 1]
+                                    }</div>
+                                </article>
+                            )
+                        }
+                    </section>
+                    :
+                    <h5>Results not simulated yet. Please go to <a href="/simulate" id={styles.simLink}>Simulate</a> tab</h5>
+            }
         </div>
     )
 }
