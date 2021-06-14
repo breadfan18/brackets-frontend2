879994741f27da361d2c395a06d911f4e3f96213
Swaroop <breadfan18@gmail.com>
2021-06-14 18:06:41 -0400

    simple styling of landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,10 +5,43 @@ const Home = (props) => (
         <article id={style.userCard}>
             <h5 className={style.welcomeName}>Welcome {props.username}</h5>
             <img
-                style={{ height: '10rem', borderRadius: '20px' }}
+                style={{ height: '10rem', borderRadius: '20px', border: '5px solid gray' }}
                 src={props.userImg}
-                alt={props.username} 
+                alt={props.username}
             />
+            <form>
+                <p className={style.pickStatusContainer}>
+                    <label>
+                        <input type="checkbox" class="filled-in" checked="checked" />
+                        <span className={style.picksStatusLabel}>Group Stage Picks Made</span>
+                    </label>
+                </p>
+                <p className={style.pickStatusContainer}>
+                    <label>
+                        <input type="checkbox" class="filled-in" checked="checked" />
+                        <span className={style.picksStatusLabel}>Group Results Simulated</span>
+                    </label>
+                </p>
+                <p className={style.pickStatusContainer}>
+                    <label>
+                        <input type="checkbox" class="filled-in" checked="checked" />
+                        <span className={style.picksStatusLabel}>Group Stage Points Calculated</span>
+                    </label>
+                </p>
+                <p className={style.pickStatusContainer}>
+                    <label>
+                        <input type="checkbox" class="filled-in" />
+                        <span className={style.picksStatusLabel}>KnockOut Rounds Picks Made</span>
+                    </label>
+                </p>
+                <p className={style.pickStatusContainer}>
+                    <label>
+                        <input type="checkbox" class="filled-in" />
+                        <span className={style.picksStatusLabel}>Knockout Roudns Results Simulated</span>
+                    </label>
+                </p>
+            </form>
+
         </article>
     </div>
 )
